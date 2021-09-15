51a7cf39510990d17a3b4a4c7b5aada8dace0179
Emma <esunderl@villanova.edu>
2021-09-15 06:29:56 -0400

    Start vizard project with basic assets

---
+++ VizIntro.py
@@ -0,0 +1,17 @@
+﻿import viz
+
+#Enable full screen anti-aliasing (FSAA) to smooth edges
+viz.setMultiSample(4)
+
+viz.go()
+
+#Increase the Field of View
+viz.MainWindow.fov(60)
+
+stadium = viz.addChild('stadium1.fbx')
+
+ball = viz.addChild('ball.fbx')
+
+viz.MainView.move([0,0,80])
+
+
