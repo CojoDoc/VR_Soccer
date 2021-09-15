2e78da519a5b0dd171181deaa86e3300c38b44a4
Emma <esunderl@villanova.edu>
2021-09-15 08:14:08 -0400

    Add avatar animation demo

--- VizIntro.py
+++ VizIntro.py
@@ -8,10 +8,24 @@ viz.go()
 #Increase the Field of View
 viz.MainWindow.fov(60)
 
+#Add assets
 stadium = viz.addChild('stadium1.fbx')
 
 ball = viz.addChild('ball.fbx')
 
+#change view point of scenary 
 viz.MainView.move([0,0,80])
 
+#Add avatar
+male = viz.addAvatar('vcc_male.cfg')
+male.setPosition([0, 0, 85])
+male.setEuler([0,0,0])
+
+#Add avatar animation
+def walkAvatars():
+
+    walk2 = vizact.walkTo([0,0,95])
+    male.addAction(walk2)
+
+vizact.onkeydown('w',walkAvatars)
 
